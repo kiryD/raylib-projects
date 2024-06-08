34e841ae08bf559f9a33d0cf676ff414b90204ec
kiryD <yaroslavdavydkin938@gmail.com>
2024-06-08 12:50:11 +0700

    modified:   Eg/main.cpp 	modified:   Eg/x64/Debug/Eg.ilk 	modified:   Eg/x64/Debug/main.obj 	modified:   Eg/x64/Debug/vc143.idb 	modified:   Eg/x64/Debug/vc143.pdb 	modified:   x64/Debug/Eg.exe 	modified:   x64/Debug/Eg.pdb

--- Eg/main.cpp
+++ Eg/main.cpp
@@ -11,7 +11,7 @@ int main()
     const int screenWidth = 1280;
     const int screenHeight = 720;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera first person");
+    InitWindow(screenWidth, screenHeight, "Eg by 'palma' and kiryD");
 
     // Camera position
     Camera3D camera = { 0 };
@@ -20,83 +20,62 @@ int main()
     camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
     camera.fovy = 90.0f;
     camera.projection = CAMERA_PERSPECTIVE;
+    BoundingBox camera_rect;
+    camera_rect.min = Vector3{ 0.0f, 0.0f, 0.0f };
+    camera_rect.max = Vector3{ 1.0f, 2.0f, 1.0f };
+    
 
     int cameraMode = CAMERA_FIRST_PERSON;
-
+    // Making THE EGG
+    Model egg_model = LoadModel("egg.obj");
+    Model stand_model = LoadModel("stand.obj");
+    Vector3 position = { 0.0f, 1.0f, 0.0f };                    // Set model position
 
     DisableCursor();
 
     SetTargetFPS(60);
-
+    
     while (!WindowShouldClose())
     {
-        if (IsKeyPressed(KEY_ONE))
-        {
-            cameraMode = CAMERA_FREE;
-            camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
-        }
-
-        if (IsKeyPressed(KEY_TWO))
+        camera_rect.min = Vector3{ camera.position.x - 1.0f, camera.position.y - 2.0f, camera.position.z - 1.0f };
+        camera_rect.max = Vector3{ camera.position.x + 2.0f, camera.position.y + 2.0f, camera.position.z + 2.0f };
+        //if(CheckCollisionBoxes(camera_rect)
+        if (IsKeyPressed(KEY_R))
         {
             cameraMode = CAMERA_FIRST_PERSON;
             camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
         }
-
-        if (IsKeyPressed(KEY_THREE))
-        {
-            cameraMode = CAMERA_THIRD_PERSON;
-            camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
+        if (IsKeyDown(KEY_LEFT_SHIFT)) {
+            camera.position.y = 1.0f;
+            camera.target.y = 1.0f;
         }
-
-        if (IsKeyPressed(KEY_FOUR))
-        {
-            cameraMode = CAMERA_ORBITAL;
-            camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
-            camera.position = Vector3{ 0.0f, 2.0f, 4.0f };
-            camera.target = Vector3{ 0.0f, 2.0f, 0.0f };
-            camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
-
-        }
-
-        // Switch camera projection
-        if (IsKeyPressed(KEY_P))
-        {
-            if (camera.projection == CAMERA_PERSPECTIVE)
-            {
-                // Create isometric view
-                cameraMode = CAMERA_THIRD_PERSON;
-                // Note: The target distance is related to the render distance in the orthographic projection
-                camera.position = Vector3{ 0.0f, 2.0f, -100.0f };
-                camera.target = Vector3{ 0.0f, 2.0f, 0.0f };
-                camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
-                camera.projection = CAMERA_ORTHOGRAPHIC;
-                camera.fovy = 20.0f; // near plane width in CAMERA_ORTHOGRAPHIC
-                CameraYaw(&camera, -135 * DEG2RAD, true);
-                CameraPitch(&camera, -45 * DEG2RAD, true, true, false);
-            }
-            else if (camera.projection == CAMERA_ORTHOGRAPHIC)
-            {
-                // Reset to default view
-                cameraMode = CAMERA_THIRD_PERSON;
-                camera.position = Vector3{ 0.0f, 2.0f, 10.0f };
-                camera.target = Vector3{ 0.0f, -2.0f, 0.0f };
-                camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
-                camera.projection = CAMERA_PERSPECTIVE;
-                camera.fovy = 60.0f;
-            }
+        if (IsKeyReleased(KEY_LEFT_SHIFT)) {
+            camera.position.y = 2.0f;
+            camera.target.y = 2.0f;
         }
         UpdateCamera(&camera, cameraMode);                  // Update camera
         BeginDrawing();
-
+        
         ClearBackground(RAYWHITE);
 
         BeginMode3D(camera);
 
+        // Draw egg and stand
+        DrawModel(egg_model, position, 0.3f, Fade(WHITE, 2.0f));
+        DrawModel(stand_model, position, 0.3f, BLACK);
+        
         DrawPlane(Vector3{ 0.0f, 0.0f, 0.0f }, Vector2{ 32.0f, 32.0f }, LIGHTGRAY); // Draw ground
-        DrawCube(Vector3{ -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLUE);     // Draw a blue wall
+        DrawCube(Vector3{ -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, SKYBLUE);     // Draw a blue wall
         DrawCube(Vector3{ 16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, LIME);      // Draw a green wall
         DrawCube(Vector3{ 0.0f, 2.5f, 16.0f }, 32.0f, 5.0f, 1.0f, GOLD);      // Draw a yellow wall
-
+        DrawCube(Vector3{ 0.0f, 2.5f, -16.0f }, 32.0f, 5.0f, 1.0f, RED);      // Draw a red wall
+        BoundingBox cube;
+        cube.min = Vector3{ 16.0f, 2.5f, 0.0f };
+        cube.max = Vector3{ 17.0f, 7.5f, 32.0f };
+        DrawBoundingBox(cube, RED);
+        DrawCube(Vector3{ 0.0f, 0.1f, 0.0f }, 1.0f, 1.0f, 1.0f, Fade(GREEN, 0.5f));
+        
+        DrawBoundingBox(camera_rect, RED);
         // Draw player cube
         if (cameraMode == CAMERA_THIRD_PERSON)
         {
@@ -107,36 +86,20 @@ int main()
         EndMode3D();
 
         // Draw info boxes
-        DrawRectangle(5, 5, 330, 100, Fade(SKYBLUE, 0.5f));
-        DrawRectangleLines(5, 5, 330, 100, BLUE);
+        DrawRectangle(5, 5, 205, 55, Fade(SKYBLUE, 0.5f));
+        DrawRectangleLines(5, 5, 205, 55, BLUE);
 
-        DrawText("Camera controls:", 15, 15, 10, BLACK);
-        DrawText("- Move keys: W, A, S, D, Space, Left-Ctrl", 15, 30, 10, BLACK);
+        DrawText("Controls:", 15, 15, 10, BLACK);
+        DrawText("- Move keys: W, A, S, D, Shift", 15, 30, 10, BLACK);
         DrawText("- Look around: arrow keys or mouse", 15, 45, 10, BLACK);
-        DrawText("- Camera mode keys: 1, 2, 3, 4", 15, 60, 10, BLACK);
-        DrawText("- Zoom keys: num-plus, num-minus or mouse scroll", 15, 75, 10, BLACK);
-        DrawText("- Camera projection key: P", 15, 90, 10, BLACK);
-
-        DrawRectangle(600, 5, 195, 100, Fade(SKYBLUE, 0.5f));
-        DrawRectangleLines(600, 5, 195, 100, BLUE);
-
-        DrawText("Camera status:", 610, 15, 10, BLACK);
-        DrawText(TextFormat("- Mode: %s", (cameraMode == CAMERA_FREE) ? "FREE" :
-            (cameraMode == CAMERA_FIRST_PERSON) ? "FIRST_PERSON" :
-            (cameraMode == CAMERA_THIRD_PERSON) ? "THIRD_PERSON" :
-            (cameraMode == CAMERA_ORBITAL) ? "ORBITAL" : "CUSTOM"), 610, 30, 10, BLACK);
-        DrawText(TextFormat("- Projection: %s", (camera.projection == CAMERA_PERSPECTIVE) ? "PERSPECTIVE" :
-            (camera.projection == CAMERA_ORTHOGRAPHIC) ? "ORTHOGRAPHIC" : "CUSTOM"), 610, 45, 10, BLACK);
-        DrawText(TextFormat("- Position: (%06.3f, %06.3f, %06.3f)", camera.position.x, camera.position.y, camera.position.z), 610, 60, 10, BLACK);
-        DrawText(TextFormat("- Target: (%06.3f, %06.3f, %06.3f)", camera.target.x, camera.target.y, camera.target.z), 610, 75, 10, BLACK);
-        DrawText(TextFormat("- Up: (%06.3f, %06.3f, %06.3f)", camera.up.x, camera.up.y, camera.up.z), 610, 90, 10, BLACK);
-
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
+    UnloadModel(egg_model);
+    UnloadModel(stand_model);
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
